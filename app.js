544d6414bf5d470bde068acb9b60785a7dc5119d
LeeKyuSung <ks5050577@gmail.com>
2023-03-01 01:04:09 +0900

    3.5 More Events

--- app.js
+++ app.js
@@ -1,2 +1,30 @@
-const title = document.querySelectorAll(".hello h1");
+const title = document.querySelector(".hello:first-child h1");
 console.log(title);
+
+function handleClick() {
+  title.style.color = "blue";
+}
+function handleMouseEnter() {
+  title.innerText = "Mouse is here!";
+}
+function handleMouseLeave() {
+  title.innerText = "Mouse is gone!";
+}
+
+title.addEventListener("click", handleClick);
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
+
+function handleResize() {
+  document.body.style.backgroundColor = "tomato";
+}
+window.addEventListener("resize", handleResize);
+
+function handleOffline() {
+  console.log("Bye bye");
+}
+function handleOnline() {
+  console.log("Welcome back");
+}
+window.addEventListener("offline", handleOffline);
+window.addEventListener("online", handleOnline);
